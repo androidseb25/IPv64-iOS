cd695a693c634ee6fb1accdef6b08a53c72d8b52
Sebastian <androidseb25@users.noreply.github.com>
2025-10-18 11:49:53 +0200

    Clean new Project

---
+++ IPv64/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IPv64
+//
+//  Created by Sebastian Rank on 18.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IPv64/IPv64App.swift
@@ -0,0 +1,17 @@
+//
+//  IPv64App.swift
+//  IPv64
+//
+//  Created by Sebastian Rank on 18.10.25.
+//
+
+import SwiftUI
+
+@main
+struct IPv64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
